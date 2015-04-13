096971fcc5aab7492ca05f595a3926a0dce10339
James Miller <james.fl.miller@live.co.uk>
2015-04-13 15:10:30 +0100

    Added counter test for DivByThree

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -1,7 +1,14 @@
 describe ('Javabuzz', function(){
   var javabuzz;
+
   it('knows when a number is divisible by three', function(){
     javabuzz = new Javabuzz;
     expect(javabuzz.isDivisibleByThree(3)).toEqual(true);
   });
+
+  it('knows when a number is not divisible by three', function(){
+    javabuzz = new Javabuzz;
+    expect(javabuzz.isDivisibleByThree(1)).toEqual(false);
+  });
+
 });

f7bc6caad37e2272b63f14cee3e3f586010eba62
James Miller <james.fl.miller@live.co.uk>
2015-04-13 15:20:00 +0100

    Solved test and added new test for DivBy15

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -1,23 +1,28 @@
 describe ('Javabuzz', function(){
+
   var javabuzz;
 
-  it('knows when a number is divisible by three', function(){
+  beforeEach(function() {
     javabuzz = new Javabuzz;
+  });
+
+  it('knows when a number is divisible by three', function() {
     expect(javabuzz.isDivisibleByThree(3)).toEqual(true);
   });
 
-  it('knows when a number is not divisible by three', function(){
-    javabuzz = new Javabuzz;
+  it('knows when a number is not divisible by three', function() {
     expect(javabuzz.isDivisibleByThree(1)).toEqual(false);
   });
 
-  it('knows that a number is divisible by five', function(){
-    javabuzz = new Javabuzz;
+  it('knows that a number is divisible by five', function() {
     expect(javabuzz.isDivisibleByFive(5)).toEqual(true);
   });
 
-  it('knows that a number is not divisible by five', function(){
-    javabuzz = new Javabuzz;
+  it('knows that a number is not divisible by five', function() {
     expect(javabuzz.isDivisibleByFive(1)).toEqual(false);  
+  });
+
+  it('knows that a number is divisible by fifteen', function() {
+    expect(javabuzz.isDivisibleByFifteen(15)).toEqual(true);
   });
 });
